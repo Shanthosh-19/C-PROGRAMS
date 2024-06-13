061d9896798ff7f96bdd7110e20976c9c16eab03
Shanthosh-19 <105621381+Shanthosh-19@users.noreply.github.com>
2024-06-13 23:07:19 +0530

    Update L4_P56.c

--- L4_P56.c
+++ L4_P56.c
@@ -1,3 +1,8 @@
+// Create a link list for the following structure and get user inputs of
+// id, Maths mark, Science Mark. Once the user inputs id as -1 exit the
+// input mode and display all the entries.
+// DATE:13|03|2024
+
 #include<stdio.h>
 
 int main(){
